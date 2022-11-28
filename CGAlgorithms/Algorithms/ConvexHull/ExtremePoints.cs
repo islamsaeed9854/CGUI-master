9d4b463773537f7eed4d6671dcbd0ab18cf097e1
Amr Wael <amr.wael.dev@gmail.com>
2022-11-28 14:47:20 +0200

    Add files via upload

--- CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
@@ -1,6 +1,7 @@
 ﻿using CGUtilities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,34 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+            for (int i = 0; i < points.Count; i++)
+                for (int j = 0; j < points.Count; j++)
+                    if (j != i)
+                        for (int k = 0; k < points.Count; k++)
+                            if (k != j && k != i)
+                                for (int l = 0; l < points.Count; l++)
+                                    if (l != k && l != j && l != i)
+                                        if (HelperMethods.PointInTriangle(points[l], points[i], points[j], points[k]) != Enums.PointInPolygon.Outside)
+                                        {
+                                            points.Remove(points[l]);
 
+                                            if (Shiftable(l, i)) i -= 1;
+                                            if (Shiftable(l, j)) j -= 1;
+                                            if (Shiftable(l, k)) k -= 1;
+                                        }
+
+            outPoints = points;
         }
+
 
         public override string ToString()
         {
             return "Convex Hull - Extreme Points";
+        }
+
+        private bool Shiftable(int anchor, int index)
+        {
+            return index >= anchor;
         }
     }
 }
